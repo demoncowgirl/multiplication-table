24aa83daab67ef038359166275d00d88eaa756f4
Beth Salvatore <bethsalvatore2018@gmail.com>
2018-06-12 21:51:31 -0400

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var new Vue ({
+    el: '#main',
+    data:{
+      numberOfRows: 5;
+      numberOfColumns: 7;
+      maxRows: 20,
+      maxColumns: 20,
+
+  },
+    //       watch: {
+    //         multFunction: function (val) {
+    //           var result = this.rows * this.columns
+    //         }
+    //     }
+    // }
+  });
